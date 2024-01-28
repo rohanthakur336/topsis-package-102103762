90d53c5d086acb9ff7317cd4b8e03c9e88c898d7
Rohan Thakur <131163309+rohanthakur336@users.noreply.github.com>
2024-01-29 03:08:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'topsis-102103762',         # How you named your package folder (MyLib)
-  packages = ['topsis-102103762'],   # Chose the same as "name"
+  name = 'topsis_102103762',         # How you named your package folder (MyLib)
+  packages = ['topsis_102103762'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
